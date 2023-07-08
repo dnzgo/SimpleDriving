b26e95c8dee0dc7a53bc235b845f05e2ff2789b7
dnzgo <dnzgzc.98@gmail.com>
2023-07-08 21:54:45 +0300

    Added Car Script
    Added Steering & Moving

---
+++ SimpleDriving/Assets/Scripts/Car.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float speedGainPerSecond = .2f;
+
+    [SerializeField] private float turnSpeed = 200f;
+
+    private float steerValue;
+
+    void Update()
+    {
+        speed += speedGainPerSecond * Time.deltaTime;
+
+        transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f); 
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    public void Steer(int value)
+    {
+        steerValue = value;
+    }
+
+}
