715c8ca5093add67b863a0fd43bef6b95b031f68
dnzgo <dnzgzc.98@gmail.com>
2023-07-12 11:47:04 +0300

    Added HighScore
    Added HighScore and edited the main menu

--- SimpleDriving/Assets/Scripts/ScoreSystem.cs
+++ SimpleDriving/Assets/Scripts/ScoreSystem.cs
@@ -8,14 +8,28 @@ public class ScoreSystem : MonoBehaviour
     [SerializeField] TMP_Text scoreText;
     [SerializeField] float scoreMultiplier;
 
+    public const string highScoreKey = "HighScore";
+
     private float score;
 
     void Update()
     {
 
-        score += Time.deltaTime * scoreMultiplier; // score increase by seconds
+        score += Time.deltaTime * scoreMultiplier; // score increase by (seconds * multiplier)
 
         scoreText.text = Mathf.FloorToInt(score).ToString();
 
     }
+
+    private void OnDestroy()
+    {
+        int currentHighScore = PlayerPrefs.GetInt(highScoreKey, 0);
+
+        if (score > currentHighScore)
+        {
+            PlayerPrefs.SetInt(highScoreKey, Mathf.FloorToInt(score));
+        }
+
+    }
+
 }
