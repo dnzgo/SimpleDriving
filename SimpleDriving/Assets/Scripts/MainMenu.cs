18d990c46cd0b352efede43803650fa1f920f037
dnzgo <dnzgzc.98@gmail.com>
2023-07-17 21:55:57 +0300

    Added Energy System

--- SimpleDriving/Assets/Scripts/MainMenu.cs
+++ SimpleDriving/Assets/Scripts/MainMenu.cs
@@ -3,19 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] TMP_Text highScoreText;
+    [SerializeField] TMP_Text energyText;
+    [SerializeField] private int maxEnergy;
+    [SerializeField] private int energyRechargeDuration;
+
+    private int energy;
+
+    private const string energyKey = "Energy";
+    private const string energyReadyKey = "EnergyReady";
 
     private void Start()
     {
         int highScore = PlayerPrefs.GetInt(ScoreSystem.highScoreKey, 0);
         highScoreText.text = $"High Score: {highScore}";
+
+        energy = PlayerPrefs.GetInt(energyKey, maxEnergy);
+
+        if (energy == 0)
+        {
+            string energyReadyString = PlayerPrefs.GetString(energyReadyKey, string.Empty);
+
+            if (energyReadyString == string.Empty) { return; }
+
+            DateTime energyReady = DateTime.Parse(energyReadyString);
+
+            if (DateTime.Now > energyReady)
+            {
+                energy = maxEnergy;
+
+                PlayerPrefs.SetInt(energyKey, energy);
+            }
+
+        }
+
+        energyText.text = $"Energy: {energy}";
+
     }
 
     public void Play()
     {
+        if (energy < 1) { return; }
+
+        energy--;
+        PlayerPrefs.SetInt(energyKey, energy);
+
+        if (energy == 0)
+        {
+            DateTime energyReady = DateTime.Now.AddMinutes(1);
+            PlayerPrefs.SetString(energyReadyKey, energyReady.ToString());
+        }
+
         SceneManager.LoadScene(1);
+
     }
 }
